58c8f9ac407e7b28a410b717d71e3735dc0d7795
daisyzl <@_@dream0712>
2019-07-06 22:48:33 +0800

    粉丝数据库

--- app/models.py
+++ app/models.py
@@ -10,6 +10,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db, login
 
+# 除了外键没有其他数据的辅助表
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 # Flask-Login插件需要在用户模型上实现某些属性和方法
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,6 +27,12 @@ class User(UserMixin, db.Model):
     # 调用它将返回给该用户动态的用户实例。
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    # chapter8 粉丝
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -36,10 +48,34 @@ class User(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
 
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    # 查询来检查两个用户之间的关系是否已经存在
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
+
+# 用户会话是Flask分配给每个连接到应用的用户的存储空间，Flask-Login通过在用户会话中存储其唯一标识符来跟踪登录用户。
+# 每当已登录的用户导航到新页面时，Flask-Login将从会话中检索用户的ID，然后将该用户实例加载到内存中。
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+# 用户登录
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
